a6250bb0c7206f592b51c256a8cda862a5bf5ff7
Santiago Del Rey Juárez <santiago.del.rey@est.fib.upc.edu>
2020-03-20 09:39:08 +0100

    Configure files for heroku

--- .github/scripts/decrypt_key.sh
+++ .github/scripts/decrypt_key.sh
@@ -2,4 +2,5 @@
 
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASE_PASSPHRASE" --output my-pet-care-85883-firebase-adminsdk-voovm-76b1b008f0.json my-pet-care-85883-firebase-adminsdk-voovm-76b1b008f0.json.gpg
+# gpg --quiet --batch --yes --decrypt --passphrase="$FIREBASE_PASSPHRASE" --output my-pet-care-85883-firebase-adminsdk-voovm-76b1b008f0.json my-pet-care-85883-firebase-adminsdk-voovm-76b1b008f0.json.gpg
+dir
