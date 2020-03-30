d10353d6ef4071ec29ea26930d589e829532c53e
Santiago Del Rey Juárez <santiago.del.rey@est.fib.upc.edu>
2020-03-30 14:48:59 +0200

    Implement upload and download

--- src/main/java/org/pesmypetcare/webservice/dao/StorageDaoImpl.java
+++ src/main/java/org/pesmypetcare/webservice/dao/StorageDaoImpl.java
@@ -1,13 +1,22 @@
 package org.pesmypetcare.webservice.dao;
 
+import com.google.cloud.storage.Bucket;
+import org.pesmypetcare.webservice.firebaseservice.FirebaseFactory;
+
 public class StorageDaoImpl implements StorageDao {
+    private Bucket storageBucket;
+
+    public StorageDaoImpl() {
+        storageBucket = FirebaseFactory.getInstance().getStorage();
+    }
+
     @Override
     public void uploadImage(String id, String imgName, byte[] img) {
-
+        storageBucket.create(id + "-" + imgName, img);
     }
 
     @Override
     public byte[] downloadImage(String id, String imgName) {
-        return new byte[0];
+        return storageBucket.get(id + "-" + imgName).getContent();
     }
 }
