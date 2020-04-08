2c7b5c8b66ee9d9bdf744a788e5ad32b3622483f
Santiago Del Rey <59149491+santidrj@users.noreply.github.com>
2020-04-08 19:10:13 +0200

    Add missing import

--- src/main/java/org/pesmypetcare/webservice/controller/usermanager/UserRestController.java
+++ src/main/java/org/pesmypetcare/webservice/controller/usermanager/UserRestController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/users")
 public class UserRestController {
