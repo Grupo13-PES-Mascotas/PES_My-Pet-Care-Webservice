f6c2657943997cd1a7032f9dadf6caf2282c0ac7
Oriol Catalan <oriol.catalan.fernandez@est.fib.upc.edu>
2020-04-14 16:04:41 +0200

    New branch done 2.0.

---
+++ src/main/java/org/pesmypetcare/webservice/controller/petmanager/WeightRestController.java
@@ -0,0 +1,115 @@
+package org.pesmypetcare.webservice.controller.petmanager;
+
+
+import org.pesmypetcare.webservice.service.WeightService;
+import org.pesmypetcare.webservice.entity.WeightEntity;
+import org.pesmypetcare.webservice.error.DatabaseAccessException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/weight")
+public class WeightRestController {
+    @Autowired
+    private WeightService weightService;
+
+    /**
+     * Creates a weight of the pet on the database.
+     * @param owner Username of the owner of the pet
+     * @param petName Name of the pet
+     * @param weightEntity The parameters of weight
+     */
+    @PostMapping("/{owner}/{petName}/{petDate}")
+    public void createWeight(@PathVariable String owner, @PathVariable String petName, @PathVariable String petDate,
+                             @RequestBody WeightEntity weightEntity) {
+        weightService.createWeight(owner, petName, petDate, weightEntity);
+    }
+    /**
+     * Deletes all weights of the pet.
+     * @param owner Username of the owner of the pet
+     * @param petName Name of the pet
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @DeleteMapping("/{owner}/{petName}")
+    public void deleteAllWeights(@PathVariable String owner, @PathVariable String petName)
+        throws DatabaseAccessException {
+        weightService.deleteAllWeights(owner, petName);
+    }
+    /**
+     * Deletes the weight with the specified pet and date.
+     * @param owner Username of the owner of the pet
+     * @param petName Name of the pet
+     * @param petDate Date of the weight instance
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @DeleteMapping("/{owner}/{petName}/{petDate}")
+    public void deleteWeightByDate(@PathVariable String owner, @PathVariable String petName,
+                                   @PathVariable String petDate) throws DatabaseAccessException {
+        weightService.deleteWeightByDate(owner, petName, petDate);
+    }
+    /**
+     * Get one weight of the pet with the specified date.
+     * @param owner Username of the owner of the pet
+     * @param petName Name of the pet
+     * @param petDate Date of the weight instance
+     * @return The WeightEntity with the data of weight
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @GetMapping("/{owner}/{petName}/{petDate}")
+    public WeightEntity getWeightByDate(@PathVariable String owner, @PathVariable String petName,
+                                        @PathVariable String petDate) throws DatabaseAccessException {
+        return weightService.getWeightByDate(owner, petName, petDate);
+    }
+    /**
+     * Gets all the specified weights of one pet.
+     * @param owner Username of the owner of the pet
+     * @param petName Name of the pet
+     * @return The list with all of the weights
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @GetMapping("/{owner}/{petName}")
+    public List<Map<String, Object>> getAllWeight(@PathVariable String owner, @PathVariable String petName)
+        throws DatabaseAccessException {
+        return weightService.getAllWeight(owner, petName);
+    }
+    /**
+     * Gets the data from all the meals eaten by the pet between the initial and final date not including them.
+     * @param owner Username of the owner of the pets
+     * @param petName Name of the pet
+     * @param initialDate Initial Date
+     * @param finalDate Final Date
+     * @return The List containing all the weights in the specified time
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @GetMapping("/{owner}/{petName}/between/{initialDate}/{finalDate}")
+    public List<Map<String, Object>> getAllWeightsBetween(@PathVariable String owner, @PathVariable String petName,
+                                                          @PathVariable String initialDate,
+                                                          @PathVariable String finalDate)
+        throws DatabaseAccessException {
+        return weightService.getAllWeightsBetween(owner, petName, initialDate, finalDate);
+    }
+    /**
+     * Updates one of the weights specified with the date and the pet name.
+     * @param owner Username of the owner of the pet
+     * @param petName Name of the pet
+     * @param petDate Date of the weight instance
+     * @param valueMap Entity that contains the value that the field will have. The new field value needs to have the
+     *                 key "value"
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @PutMapping("/{owner}/{petName}/{petDate}")
+    public void updateWeight(@PathVariable String owner, @PathVariable String petName, @PathVariable String petDate,
+                             @RequestBody Map<String, Object> valueMap) throws DatabaseAccessException {
+        weightService.updateWeight(owner, petName, petDate, valueMap.get("value"));
+    }
+}
---
+++ src/main/java/org/pesmypetcare/webservice/dao/WeightDaoImpl.java
@@ -0,0 +1,166 @@
+package org.pesmypetcare.webservice.dao;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import org.pesmypetcare.webservice.entity.WeightEntity;
+import org.pesmypetcare.webservice.error.DatabaseAccessException;
+import org.pesmypetcare.webservice.firebaseservice.FirebaseFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class WeightDaoImpl implements WeightDao {
+    private final String DELFAIL_KEY;
+    private final String WEIGHT_DOES_NOT_EXIST_EXC;
+    private final String INVALID_WEIGHT_EXC;
+    private final String internalListString1;
+    private final String internalListString2;
+
+
+    private Firestore db;
+
+
+    public WeightDaoImpl() {
+        db = FirebaseFactory.getInstance().getFirestore();
+
+        DELFAIL_KEY = "deletion-failed";
+        WEIGHT_DOES_NOT_EXIST_EXC = "The weight does not exist";
+        INVALID_WEIGHT_EXC = "invalid-pet";
+        internalListString1 = "date";
+        internalListString2 = "body";
+    }
+
+    @Override
+    public void createWeight(String owner, String petName, String date, WeightEntity weightEntity) {
+        DocumentReference weightRef = getWeightsRef(owner, petName).document(date);
+        weightRef.set(weightEntity);
+        System.out.println("Weight created");
+    }
+
+    @Override
+    public void deleteAllWeights(String owner, String petName) throws DatabaseAccessException {
+        CollectionReference weightsRef = getWeightsRef(owner, petName);
+        try {
+            ApiFuture<QuerySnapshot> future = weightsRef.get();
+            List<QueryDocumentSnapshot> weightDocuments = future.get().getDocuments();
+            for (QueryDocumentSnapshot weightDocument : weightDocuments) {
+                weightDocument.getReference().delete();
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new DatabaseAccessException(DELFAIL_KEY, e.getMessage());
+        }
+    }
+
+    @Override
+    public void deleteWeightByDate(String owner, String petName, String petDate) {
+        DocumentReference weightRef = getWeightsRef(owner, petName).document(petDate);
+        weightRef.delete();
+    }
+
+    @Override
+    public WeightEntity getWeightByDate(String owner, String petName, String petDate) throws DatabaseAccessException {
+        CollectionReference weightsRef = getWeightsRef(owner, petName);
+        DocumentReference weightRef = weightsRef.document(petDate);
+        ApiFuture<DocumentSnapshot> future = weightRef.get();
+        DocumentSnapshot mealDoc;
+        try {
+            mealDoc = future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new DatabaseAccessException(DELFAIL_KEY, e.getMessage());
+        }
+        if (!mealDoc.exists()) {
+            throw new DatabaseAccessException(INVALID_WEIGHT_EXC, WEIGHT_DOES_NOT_EXIST_EXC);
+        }
+        return mealDoc.toObject(WeightEntity.class);
+    }
+
+    @Override
+    public List<Map<String, Object>> getAllWeight(String owner, String petName) throws DatabaseAccessException {
+        CollectionReference weightsRef = getWeightsRef(owner, petName);
+        List<Map<String, Object>> externalList = new ArrayList<>();
+        try {
+            getAllWeightsOfAPetFromDatabase(weightsRef, externalList);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new DatabaseAccessException(DELFAIL_KEY, e.getMessage());
+        }
+        return externalList;
+    }
+
+    @Override
+    public List<Map<String, Object>> getAllWeightsBetween(String owner, String petName, String initialDate,
+                                                          String finalDate) throws DatabaseAccessException {
+        CollectionReference weightsRef = getWeightsRef(owner, petName);
+        List<Map<String, Object>> externalList = new ArrayList<>();
+        try {
+            getWeightsBetweenDatesFromDatabase(initialDate, finalDate, weightsRef, externalList);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new DatabaseAccessException(DELFAIL_KEY, e.getMessage());
+        }
+        return externalList;
+    }
+
+    @Override
+    public void updateWeight(String owner, String petName, String petDate, Object value) {
+        CollectionReference weightsRef = getWeightsRef(owner, petName);
+        DocumentReference weightRef = weightsRef.document(petDate);
+        weightRef.update("weightValue", value);
+    }
+
+    public CollectionReference getWeightsRef(String owner, String petName) {
+        return db.collection("users").document(owner).collection("pets").document(petName)
+            .collection("weights");
+    }
+
+    /**
+     * Gets all the weights of the collection and puts them in the externalList.
+     * @param weightsRef Reference to the collection of weights
+     * @param externalList list that will contain all the weights
+     * @throws InterruptedException Exception thrown by the DB if the operation is interrupted
+     * @throws ExecutionException Exception thrown by the DB if there's an execution problem
+     */
+    private void getAllWeightsOfAPetFromDatabase(CollectionReference weightsRef, List<Map<String, Object>> externalList)
+        throws InterruptedException, ExecutionException {
+        ApiFuture<QuerySnapshot> future = weightsRef.get();
+        List<QueryDocumentSnapshot> weightDocuments = future.get().getDocuments();
+        for (QueryDocumentSnapshot weightDocument : weightDocuments) {
+            Map<String, Object> internalList = new HashMap<>();
+            internalList.put(internalListString1, weightDocument.getId());
+            internalList.put(internalListString2, weightDocument.toObject(WeightEntity.class));
+            externalList.add(internalList);
+        }
+    }
+
+    /**
+     * Gets all the weights of the collection between the initial and final dates without taking them into account and
+     * puts them in the externalList.
+     * @param initialDate Initial date
+     * @param finalDate Final date
+     * @param weightsRef Reference to the collection of weights
+     * @param externalList list that will contain all the weights
+     * @throws InterruptedException Exception thrown by the DB if the operation is interrupted
+     * @throws ExecutionException Exception thrown by the DB if there's an execution problem
+     */
+    private void getWeightsBetweenDatesFromDatabase(String initialDate, String finalDate,
+                                                    CollectionReference weightsRef, List<Map<String,
+        Object>> externalList) throws InterruptedException, ExecutionException {
+        ApiFuture<QuerySnapshot> future = weightsRef.get();
+        List<QueryDocumentSnapshot> weightDocuments = future.get().getDocuments();
+        for (QueryDocumentSnapshot weightDocument : weightDocuments) {
+            String date = weightDocument.getId();
+            if (initialDate.compareTo(date) < 0 && finalDate.compareTo(date) > 0) {
+                Map<String, Object> internalList = new HashMap<>();
+                internalList.put(internalListString1, date);
+                internalList.put(internalListString2, weightDocument.toObject(WeightEntity.class));
+                externalList.add(internalList);
+            }
+        }
+    }
+}
