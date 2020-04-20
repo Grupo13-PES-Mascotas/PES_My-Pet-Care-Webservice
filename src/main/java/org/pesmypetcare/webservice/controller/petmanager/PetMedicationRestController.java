58138f6a02714b72d2315426619a14aa85b53362
AlvarTB <alvarotrius94@gmail.com>
2020-04-20 13:02:10 +0200

    Fixed Codacy Issues

--- src/main/java/org/pesmypetcare/webservice/controller/petmanager/PetMedicationRestController.java
+++ src/main/java/org/pesmypetcare/webservice/controller/petmanager/PetMedicationRestController.java
@@ -4,7 +4,15 @@ import org.pesmypetcare.webservice.entity.MedicationEntity;
 import org.pesmypetcare.webservice.error.DatabaseAccessException;
 import org.pesmypetcare.webservice.service.MedicationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
@@ -44,7 +52,7 @@ public class PetMedicationRestController {
     public void deleteByDateAndName(@RequestHeader(TOKEN) String accessToken,
                                     @PathVariable String owner,
                                     @PathVariable String petName,
-                                    @PathVariable  String dateName) {
+                                    @PathVariable String dateName) {
         medicationService.deleteByDateAndName(owner, petName, dateName);
     }
 
