4c87059bd1d51755fcd62c301f6a8c4922ad53a4
Santiago Del Rey Juárez <santiago.del.rey@est.fib.upc.edu>
2020-05-01 17:03:50 +0200

    Create DocumentPath.java
    Create a static method that builds the path to a document in a specific collection

---
+++ src/main/java/org/pesmypetcare/webservice/firebaseservice/firestore/Collections.java
@@ -0,0 +1,25 @@
+package org.pesmypetcare.webservice.firebaseservice.firestore;
+
+/**
+ * @author Santiago Del Rey
+ */
+public enum Collections {
+    groups,
+    forums,
+    messages,
+    members,
+    groupsNames,
+    forumsNames,
+    tags,
+    usernames,
+    users,
+    pets,
+    meals,
+    medications,
+    freqTrainings,
+    freqWashes,
+    weekTrainings,
+    kcals,
+    kcalsAverages,
+    weights
+}
