7421d4ac5721c6c5974da1bd011ddd5bb86bda8e
marclll <sgmarcsg@gmail.com>
2020-05-01 12:51:23 +0200

    New PetRestController functions added

--- src/main/java/org/pesmypetcare/webservice/controller/petmanager/PetRestController.java
+++ src/main/java/org/pesmypetcare/webservice/controller/petmanager/PetRestController.java
@@ -4,6 +4,7 @@ import org.pesmypetcare.webservice.entity.PetEntity;
 import org.pesmypetcare.webservice.error.DatabaseAccessException;
 import org.pesmypetcare.webservice.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -29,7 +31,9 @@ public class PetRestController {
      * @param pet The pet entity that contains the attributes of the pet
      */
     @PostMapping("/{owner}/{name}")
+    @ResponseStatus(HttpStatus.CREATED)
     public void createPet(@PathVariable String owner, @PathVariable String name, @RequestBody PetEntity pet) {
+        pet.initializeMaps();
         petService.createPet(owner, name, pet);
     }
 
@@ -40,6 +44,7 @@ public class PetRestController {
      * @throws DatabaseAccessException If an error occurs when accessing the database
      */
     @DeleteMapping("/{owner}/{name}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteByOwnerAndName(@PathVariable String owner,
                                      @PathVariable String name) throws DatabaseAccessException {
         petService.deleteByOwnerAndName(owner, name);
@@ -51,6 +56,7 @@ public class PetRestController {
      * @throws DatabaseAccessException If an error occurs when accessing the database
      */
     @DeleteMapping("/{owner}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAllPets(@PathVariable String owner) throws DatabaseAccessException {
         petService.deleteAllPets(owner);
     }
@@ -63,6 +69,7 @@ public class PetRestController {
      * @throws DatabaseAccessException If an error occurs when accessing the database
      */
     @GetMapping("/{owner}/{name}")
+    @ResponseStatus(HttpStatus.OK)
     public PetEntity getPetData(@PathVariable String owner, @PathVariable String name) throws DatabaseAccessException {
         return petService.getPetData(owner, name);
     }
@@ -74,6 +81,7 @@ public class PetRestController {
      * @throws DatabaseAccessException If an error occurs when accessing the database
      */
     @GetMapping("/{owner}")
+    @ResponseStatus(HttpStatus.OK)
     public List<Map<String, Object>> getAllPetsData(@PathVariable String owner) throws DatabaseAccessException {
         return petService.getAllPetsData(owner);
     }
@@ -86,9 +94,10 @@ public class PetRestController {
      * @return The value from the field on the database
      * @throws DatabaseAccessException If an error occurs when accessing the database
      */
-    @GetMapping("/{owner}/{name}/{field}")
-    public Object getField(@PathVariable String owner, @PathVariable String name,
-                           @PathVariable String field) throws DatabaseAccessException {
+    @GetMapping("/{owner}/{name}/simple/{field}")
+    @ResponseStatus(HttpStatus.OK)
+    public Object getSimpleField(@PathVariable String owner, @PathVariable String name,
+                                 @PathVariable String field) throws DatabaseAccessException {
         return petService.getField(owner, name, field);
     }
 
@@ -100,9 +109,126 @@ public class PetRestController {
      * @param valueMap Entity that contains the value that the field will have. The new field value needs to have the
      *                key "value"
      */
-    @PutMapping("/{owner}/{name}/{field}")
-    public void updateField(@PathVariable String owner, @PathVariable String name, @PathVariable String field,
-                            @RequestBody Map<String, Object> valueMap) {
+    @PutMapping("/{owner}/{name}/simple/{field}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateSimpleField(@PathVariable String owner, @PathVariable String name, @PathVariable String field,
+                                  @RequestBody Map<String, Object> valueMap) {
         petService.updateField(owner, name, field, valueMap.get("value"));
+    }
+
+    /**
+     * Deletes the map for the specified field of the pet on the database.
+     * @param owner Username of the owner of the pet
+     * @param name Name of the pet
+     * @param field Name of the field to delete
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @DeleteMapping("/{owner}/{name}/map/{field}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMapField(@PathVariable String owner, @PathVariable String name,
+                               @PathVariable String field) throws DatabaseAccessException {
+        petService.deleteMapField(owner, name, field);
+    }
+
+    /**
+     * Gets the map for the specified field of the pet on the database.
+     * @param owner Username of the owner of the pet
+     * @param name Name of the pet
+     * @param field Name of the field to retrieve
+     * @return The map from the field on the database
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @GetMapping("/{owner}/{name}/map/{field}")
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, Object> getMapField(@PathVariable String owner, @PathVariable String name,
+                                 @PathVariable String field) throws DatabaseAccessException {
+        return petService.getMapField(owner, name, field);
+    }
+
+    /**
+     * Adds an element to the map for the specified field of the pet on the database.
+     * @param owner Username of the owner of the pets
+     * @param name Name of the pet
+     * @param field Name of the field
+     * @param key Key of the new element to be added
+     * @param body Element to be added
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @PostMapping("/{owner}/{name}/map/{field}/{key}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addMapFieldElement(@PathVariable String owner, @PathVariable String name,
+                                        @PathVariable String field, @PathVariable String key,
+                                     @RequestBody Object body) throws DatabaseAccessException {
+        PetEntity.checkKeyAndBody(field,key,body);
+        petService.addMapFieldElement(owner, name, field, key, body);
+    }
+
+    /**
+     * Deletes an element from the map for the specified field of the pet on the database.
+     * @param owner Username of the owner of the pets
+     * @param name Name of the pet
+     * @param field Name of the field
+     * @param key Key of the element to delete
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @DeleteMapping("/{owner}/{name}/map/{field}/{key}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMapFieldElement(@PathVariable String owner, @PathVariable String name,
+                                        @PathVariable String field, @PathVariable String key)
+        throws DatabaseAccessException {
+        return petService.deleteMapFieldElement(owner, name, field, key);
+    }
+
+    /**
+     * Updates an element from the map for the specified field of the pet on the database.
+     * @param owner Username of the owner of the pets
+     * @param name Name of the pet
+     * @param field Name of the field
+     * @param key Key of the element to update
+     * @param body Update of the element
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @PutMapping("/{owner}/{name}/map/{field}/{key}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateMapFieldElement(@PathVariable String owner, @PathVariable String name,
+                                        @PathVariable String field, @PathVariable String key,
+                                      @RequestBody Object body) throws DatabaseAccessException {
+        PetEntity.checkKeyAndBody(field,key,body);
+        return petService.updateMapFieldElement(owner, name, field, key, body);
+    }
+
+    /**
+     * Gets an element from the map for the specified field of the pet on the database.
+     * @param owner Username of the owner of the pets
+     * @param name Name of the pet
+     * @param field Name of the field
+     * @param key Key of the element
+     * @return Element assigned to the key
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @GetMapping("/{owner}/{name}/map/{field}/{key}")
+    @ResponseStatus(HttpStatus.OK)
+    public Object getMapFieldElement(@PathVariable String owner, @PathVariable String name,
+                                     @PathVariable String field, @PathVariable String key)
+        throws DatabaseAccessException {
+        return petService.getMapFieldElement(owner, name, field, key);
+    }
+
+    /**
+     * Gets all the elements between the keys from the map for the specified field.
+     * @param owner Username of the owner of the pets
+     * @param name Name of the pet
+     * @param field Name of the field
+     * @param key1 Start key (This one included)
+     * @param key2 End Key (This one included)
+     * @return The map containing the elements between the keys
+     * @throws DatabaseAccessException If an error occurs when accessing the database
+     */
+    @GetMapping("/{owner}/{name}/map/{field}/{key1}/{key2}")
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, Object> getMapFieldElementsBetweenKeys(@PathVariable String owner, @PathVariable String name,
+                                     @PathVariable String field, @PathVariable String key1, @PathVariable String key2)
+        throws DatabaseAccessException {
+        return petService.getMapFieldElementsBetweenKeys(owner, name, field, key1, key2);
     }
 }
--- src/main/java/org/pesmypetcare/webservice/entity/PetEntity.java
+++ src/main/java/org/pesmypetcare/webservice/entity/PetEntity.java
@@ -2,6 +2,9 @@ package org.pesmypetcare.webservice.entity;
 
 import lombok.Data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 public class PetEntity {
     private GenderType gender;
@@ -12,4 +15,20 @@ public class PetEntity {
     private Double recommendedKcal;
     private String profileImageLocation;
     private String calendarId;
+    private Map<String, MealEntity> meals;
+    private Map<String, Integer> trainings;
+    private Map<String, Integer> washes;
+    private Map<String, Integer> weights;
+    private Map<String, Map<String, MedicationEntity>> medicaments;
+
+    public static void checkKeyAndBody(String field, String key, Object body) {
+    }
+
+    public void initializeMaps() {
+        meals = new HashMap<>();
+        trainings = new HashMap<>();
+        washes = new HashMap<>();
+        weights = new HashMap<>();
+        medicaments = new HashMap<>();
+    }
 }
