c8fdceb112d734aea343d1fb9d11a0e2f213c5f9
santidrj <santi.delrey@gmail.com>
2020-05-06 11:18:32 +0200

    Finish ForumService.java tests

--- src/test/java/org/pesmypetcare/webservice/service/communitymanager/ForumServiceTest.java
+++ src/test/java/org/pesmypetcare/webservice/service/communitymanager/ForumServiceTest.java
@@ -1,6 +1,8 @@
 package org.pesmypetcare.webservice.service.communitymanager;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,11 +11,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.pesmypetcare.webservice.dao.communitymanager.ForumDao;
 import org.pesmypetcare.webservice.dao.communitymanager.GroupDao;
 import org.pesmypetcare.webservice.entity.communitymanager.ForumEntity;
+import org.pesmypetcare.webservice.entity.communitymanager.MessageEntity;
 import org.pesmypetcare.webservice.error.DatabaseAccessException;
 import org.pesmypetcare.webservice.error.DocumentException;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.BDDMockito.given;
@@ -28,7 +36,14 @@ import static org.mockito.Mockito.verify;
 class ForumServiceTest {
     private String groupName;
     private String forumName;
+    private String newName;
+    private String token;
+    private String creator;
+    private String date;
+    private List<String> newTags;
+    private List<String> deletedTags;
     private ForumEntity forumEntity;
+    private MessageEntity messageEntity;
 
     @Mock
     private GroupDao groupDao;
@@ -44,58 +59,150 @@ class ForumServiceTest {
         forumName = "Huskies";
         forumEntity = new ForumEntity();
         forumEntity.setName(forumName);
+        token = "my-token";
+        creator = "John Doe";
+        date = "2020-05-01T17:48:15";
+        newTags = new LinkedList<>();
+        deletedTags = new LinkedList<>();
+        messageEntity = new MessageEntity();
+        newName = "German Shepherds";
     }
 
     @Test
-    public void createForum() throws DatabaseAccessException, DocumentException {
-        given(forumDao.forumNameInUse(anyString(), anyString())).willReturn(false);
-        willDoNothing().given(forumDao).createForum(anyString(), any(ForumEntity.class));
+    public void getAllForumsFromGroup() throws DatabaseAccessException, DocumentException {
+        List<ForumEntity> forumEntityList = new LinkedList<>();
+        forumEntityList.add(forumEntity);
+        given(groupDao.groupNameInUse(anyString())).willReturn(true);
+        given(service.getAllForumsFromGroup(groupName)).willReturn(forumEntityList);
 
-        service.createForum(groupName, forumEntity);
-        verify(forumDao).forumNameInUse(same(groupName),same(forumEntity.getName()));
-        verify(forumDao).createForum(same(groupName),same(forumEntity));
+        List<ForumEntity> result = service.getAllForumsFromGroup(groupName);
+        assertEquals(forumEntityList, result,
+                     "Should return a list with all the forum entities in the requested " + "collection.");
     }
 
     @Test
-    public void createForumShouldFailWhenTheForumNameAlreadyExistsInTheGroup() {
+    public void getAllForumsFromGroupShouldThrowDocumentExceptionWhenTheGroupDoesNotExist() {
         assertThrows(DocumentException.class, () -> {
-            lenient().when(forumDao.forumNameInUse(anyString(), anyString())).thenReturn(true);
-
-            service.createForum(groupName, forumEntity);
+            lenient().when(groupDao.groupNameInUse(anyString())).thenReturn(false);
+            service.getForum(groupName, forumName);
         });
     }
 
-    @Test
-    public void deleteForum() throws DatabaseAccessException, DocumentException {
-        given(forumDao.forumNameInUse(anyString(), anyString())).willReturn(true);
-        willDoNothing().given(forumDao).deleteForum(anyString(), anyString());
+    @Nested
+    class ForumNameInUseReturnsFalse {
+        @BeforeEach
+        public void setUp() throws DatabaseAccessException {
+            given(forumDao.forumNameInUse(anyString(), anyString())).willReturn(false);
+        }
 
-        service.deleteForum(groupName, forumName);
-        verify(forumDao).forumNameInUse(same(groupName),same(forumName));
-        verify(forumDao).deleteForum(same(groupName),same(forumName));
+        @Test
+        public void createForum() throws DatabaseAccessException, DocumentException {
+            willDoNothing().given(forumDao).createForum(anyString(), any(ForumEntity.class));
+
+            service.createForum(groupName, forumEntity);
+            verify(forumDao).forumNameInUse(same(groupName), same(forumEntity.getName()));
+            verify(forumDao).createForum(same(groupName), same(forumEntity));
+        }
+
+        @Test
+        public void deleteForumShouldThrowDocumentExceptionWhenTheForumDoesNotExistInTheGroup() {
+            assertThrows(DocumentException.class, () -> service.deleteForum(groupName, forumName));
+        }
+
+        @Test
+        public void getForumShouldThrowDocumentExceptionWhenTheForumDoesNotExistInTheGroup() {
+            assertThrows(DocumentException.class, () -> service.getForum(groupName, forumName));
+        }
+
+        @Test
+        public void updateNameShouldThrowDocumentExceptionWhenTheForumDoesNotExistInTheGroup() {
+            assertThrows(DocumentException.class, () -> service.updateName(groupName, forumName, newName));
+        }
+
+        @Test
+        public void updateTagsShouldThrowDocumentExceptionWhenTheForumDoesNotExistInTheGroup() {
+            assertThrows(DocumentException.class, () -> service.updateTags(groupName, forumName, newTags, deletedTags));
+        }
+
+        @Test
+        public void postMessageShouldThrowDocumentExceptionWhenTheForumDoesNotExistInTheGroup() {
+            assertThrows(DocumentException.class,
+                         () -> service.postMessage(token, groupName, forumName, messageEntity));
+        }
+
+        @Test
+        public void deleteMessageShouldThrowDocumentExceptionWhenTheForumDoesNotExistInTheGroup() {
+            assertThrows(DocumentException.class,
+                         () -> service.deleteMessage(token, groupName, forumName, creator, date));
+        }
     }
 
-    @Test
-    public void getForum() {
-    }
+    @Nested
+    class ForumNameInUseReturnsTrue {
+        @BeforeEach
+        public void setUp() throws DatabaseAccessException {
+            given(forumDao.forumNameInUse(anyString(), anyString())).willReturn(true);
+        }
 
-    @Test
-    public void getAllForumsFromGroup() {
-    }
+        @Test
+        public void createForumShouldThrowDocumentExceptionWhenTheForumNameAlreadyExistsInTheGroup() {
+            assertThrows(DocumentException.class, () -> service.createForum(groupName, forumEntity));
+        }
 
-    @Test
-    public void updateName() {
-    }
+        @Test
+        public void getForum() throws DatabaseAccessException, DocumentException {
+            given(service.getForum(groupName, forumName)).willReturn(forumEntity);
 
-    @Test
-    public void updateTags() {
-    }
+            ForumEntity result = service.getForum(groupName, forumName);
+            assertEquals(forumEntity, result, "Should return the forum entity requested.");
+        }
 
-    @Test
-    public void postMessage() {
-    }
+        @Nested
+        class VerifiesForumNameInUse {
+            @AfterEach
+            public void verifyForumNameInUse() throws DatabaseAccessException {
+                verify(forumDao).forumNameInUse(same(groupName), same(forumName));
+            }
 
-    @Test
-    public void deleteMessage() {
+            @Test
+            public void deleteForum() throws DatabaseAccessException, DocumentException {
+                willDoNothing().given(forumDao).deleteForum(anyString(), anyString());
+
+                service.deleteForum(groupName, forumName);
+                verify(forumDao).deleteForum(same(groupName), same(forumName));
+            }
+
+            @Test
+            public void updateName() throws DatabaseAccessException, DocumentException {
+                willDoNothing().given(forumDao).updateName(anyString(), anyString(), anyString());
+
+                service.updateName(groupName, forumName, newName);
+                verify(forumDao).updateName(same(groupName), same(forumName), same(newName));
+            }
+
+            @Test
+            public void updateTags() throws DatabaseAccessException, DocumentException {
+                willDoNothing().given(forumDao).updateTags(anyString(), anyString(), anyList(), anyList());
+
+                service.updateTags(groupName, forumName, newTags, deletedTags);
+                verify(forumDao).updateTags(same(groupName), same(forumName), same(newTags), same(deletedTags));
+            }
+
+            @Test
+            public void postMessage() throws DatabaseAccessException, DocumentException {
+                willDoNothing().given(forumDao).postMessage(anyString(), anyString(), any(MessageEntity.class));
+
+                service.postMessage(token, groupName, forumName, messageEntity);
+                verify(forumDao).postMessage(same(groupName), same(forumName), same(messageEntity));
+            }
+
+            @Test
+            public void deleteMessage() throws DatabaseAccessException, DocumentException {
+                willDoNothing().given(forumDao).deleteMessage(anyString(), anyString(), anyString(), anyString());
+
+                service.deleteMessage(token, groupName, forumName, creator, date);
+                verify(forumDao).deleteMessage(same(groupName), same(forumName), same(creator), same(date));
+            }
+        }
     }
 }
