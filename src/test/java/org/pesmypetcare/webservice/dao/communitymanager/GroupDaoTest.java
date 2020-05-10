2152a4afd873434ece8e1d98cdd8de6f1b4131cc
santidrj <santi.delrey@gmail.com>
2020-05-10 22:10:30 +0200

    Create GroupDaoTest and update README.md

---
+++ src/test/java/org/pesmypetcare/webservice/dao/communitymanager/GroupDaoTest.java
@@ -0,0 +1,60 @@
+package org.pesmypetcare.webservice.dao.communitymanager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Santiago Del Rey
+ */
+class GroupDaoTest {
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @Test
+    public void createGroup() {
+    }
+
+    @Test
+    public void deleteGroup() {
+    }
+
+    @Test
+    public void getGroup() {
+    }
+
+    @Test
+    public void getAllGroups() {
+    }
+
+    @Test
+    public void updateField() {
+    }
+
+    @Test
+    public void groupNameInUse() {
+    }
+
+    @Test
+    public void subscribe() {
+    }
+
+    @Test
+    public void updateTags() {
+    }
+
+    @Test
+    public void getAllTags() {
+    }
+
+    @Test
+    public void unsubscribe() {
+    }
+
+    @Test
+    public void getGroupId() {
+    }
+}
