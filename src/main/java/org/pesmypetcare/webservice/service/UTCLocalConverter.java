de1c419a0f71f1431ea63e5df82fbfd95494475f
AlvarTB <alvarotrius94@gmail.com>
2020-05-14 19:47:15 +0200

    UTCLocalConverter added but not implemented

---
+++ src/main/java/org/pesmypetcare/webservice/service/UTCLocalConverter.java
@@ -0,0 +1,32 @@
+package org.pesmypetcare.webservice.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class UTCLocalConverter {
+    public static String convertUTCtoLocal(String datein) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date = sdf.parse(datein);
+        sdf.setTimeZone(TimeZone.getDefault());
+        return sdf.format(date);
+    }
+
+    public static String convertLocaltoUTC(String datein) throws ParseException{
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        TimeZone tz = TimeZone.getDefault();
+        sdf.setTimeZone(TimeZone.getDefault());
+        Date date = sdf.parse(datein);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return sdf.format(date);
+    }
+
+    public static String getCurrentUTC(){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy'T'HH:mm:ss");
+        Date date2 = new Date();
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return sdf.format(date2);
+    }
+}
