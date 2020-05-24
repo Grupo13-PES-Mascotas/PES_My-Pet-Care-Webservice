5fb9a872ee310fe2030e932ec0af2afbc41efa58
oricat8 <oriol.catalan.fernandez@est.fib.upc.edu>
2020-05-24 16:28:03 +0200

    Icon medal started with Blob object. Medal created.

---
+++ src/main/java/org/pesmypetcare/webservice/entity/medalmanager/Medal.java
@@ -0,0 +1,16 @@
+package org.pesmypetcare.webservice.entity.medalmanager;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author Oriol Catalán
+ */
+@Data
+public class Medal {
+    private String name;
+    private List<Double> levels;
+    private String description;
+    private String medalIcon;
+}
--- src/main/java/org/pesmypetcare/webservice/entity/medalmanager/MedalEntity.java
+++ src/main/java/org/pesmypetcare/webservice/entity/medalmanager/MedalEntity.java
@@ -1,7 +1,9 @@
 package org.pesmypetcare.webservice.entity.medalmanager;
 
+import com.google.protobuf.ByteString;
 import lombok.Data;
 
+import com.google.cloud.firestore.Blob;
 import java.util.List;
 
 /**
@@ -13,19 +15,21 @@ public class MedalEntity {
     public static final String LEVELS = "levels";
     public static final String DESCRIPTION = "description";
     public static final String ICON_LOCATION = "iconLocation";
+    public static final String MEDAL_ICON = "medalIcon";
     private String name;
     private List<Double> levels;
     private String description;
-    private String iconLocation;
+    private Blob medalIcon;
 
 
 
     public MedalEntity() { }
 
-    public MedalEntity(String name, List<Double> levels, String description) {
-        this.name = name;
-        this.levels = levels;
-        this.description = description;
+    public MedalEntity(Medal medal) {
+        this.name = medal.getName();
+        this.levels = medal.getLevels();
+        this.description = medal.getDescription();
+        this.medalIcon = convertStringToByteString(medal.getMedalIcon());
     }
 
     /**
@@ -34,8 +38,17 @@ public class MedalEntity {
      */
     public static void checkField(String field) {
         if (!NAME.equals(field) && !LEVELS.equals(field) && !DESCRIPTION.equals(field)
-            && !ICON_LOCATION.equals(field)) {
+            && !ICON_LOCATION.equals(field) && !MEDAL_ICON.equals(field)) {
             throw new IllegalArgumentException("Field does not exists");
         }
+    }
+
+    /**
+     * Convert string to ByteString, and put it into a Blob instance.
+     * @param text String containing the bytes of the image
+     * @return Image converted to a Blob
+     */
+    private static Blob convertStringToByteString (String text) {
+        return Blob.fromByteString(ByteString.copyFromUtf8(text));
     }
 }
