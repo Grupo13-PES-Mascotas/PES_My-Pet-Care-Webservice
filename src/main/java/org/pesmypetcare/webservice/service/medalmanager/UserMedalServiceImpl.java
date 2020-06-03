82db33e8ba28cd0338e5a2f3a6b4a98d07bea6c3
oricat8 <oriol.catalan.fernandez@est.fib.upc.edu>
2020-06-03 17:03:38 +0200

    Create method on UserMedal

--- src/main/java/org/pesmypetcare/webservice/service/medalmanager/UserMedalServiceImpl.java
+++ src/main/java/org/pesmypetcare/webservice/service/medalmanager/UserMedalServiceImpl.java
@@ -1,7 +1,6 @@
 package org.pesmypetcare.webservice.service.medalmanager;
 
 import org.pesmypetcare.webservice.dao.medalmanager.UserMedalDao;
-import org.pesmypetcare.webservice.entity.medalmanager.MedalEntity;
 import org.pesmypetcare.webservice.entity.medalmanager.UserMedalEntity;
 import org.pesmypetcare.webservice.error.DatabaseAccessException;
 import org.pesmypetcare.webservice.error.DocumentException;
