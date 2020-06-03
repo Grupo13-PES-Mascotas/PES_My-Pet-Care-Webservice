996186f83415dc9458e7b7118955cd8a847757e4
oricat8 <oriol.catalan.fernandez@est.fib.upc.edu>
2020-06-03 20:05:24 +0200

    New methods.

--- src/main/java/org/pesmypetcare/webservice/dao/medalmanager/MedalDaoImpl.java
+++ src/main/java/org/pesmypetcare/webservice/dao/medalmanager/MedalDaoImpl.java
@@ -98,7 +98,7 @@ public class MedalDaoImpl implements MedalDao {
     private void addMedalsToUsers(MedalEntity medal) throws DatabaseAccessException, DocumentException {
         path = Path.ofCollection(Collections.used_usernames);
         List<DocumentSnapshot> allUsers = dbCol.listAllCollectionDocumentSnapshots(path);
-        UserMedalEntity medalEntity  = new UserMedalEntity(medal.getName(), 0., 0.,
+        UserMedalEntity medalEntity = new UserMedalEntity(medal.getName(), 0., 0.,
             new ArrayList<>(), new Medal(medal));
         for (DocumentSnapshot user: allUsers) {
             userMedalDao.createUserMedal(user.getId(), medal.getName(), medalEntity);
