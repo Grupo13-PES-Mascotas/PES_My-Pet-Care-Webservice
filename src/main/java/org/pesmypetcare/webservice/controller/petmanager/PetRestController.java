8f8d3f413edd3eb65e41976cea1fb72919cef327
Santiago Del Rey <59149491+santidrj@users.noreply.github.com>
2020-06-06 02:17:40 +0200

    Update PetRestController.java

--- src/main/java/org/pesmypetcare/webservice/controller/petmanager/PetRestController.java
+++ src/main/java/org/pesmypetcare/webservice/controller/petmanager/PetRestController.java
@@ -25,7 +25,7 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/pet")
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 public class PetRestController {
     @Autowired
     private PetService petService;
